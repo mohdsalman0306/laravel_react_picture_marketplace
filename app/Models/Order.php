fe9f5aace6eb5372182cde33a7485f8e94e64630
Salman <mohd.salman0306@gmail.com>
2024-05-21 22:06:38 +0400

    Added relation with Category and Order Model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['total', 'user_id', 'picture_is'];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function picture() {
+        return $this->belongsTo(Picture::class)
+    }
 }
