20d7251855b9b92dda24aca9077a9c7d84890873
Salman <mohd.salman0306@gmail.com>
2024-05-25 22:58:18 +0400

    Added Category Controller, created new store and update category Request, and some respective blade files

---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,18 @@
+<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar">
+    <div class="position-sticky pt-3">
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="{{route('admin.index')}}">
+                    <i class="fas fa-dashboard"></i>
+                    Dashboard
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="{{route('admin.categories.index')}}">
+                    <i class="fas fa-bars"></i>
+                    Categories
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\CategoryController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [AdminController::class, 'login'])->name('admin.login');
+Route::post('auth', [AdminController::class,'auth'])->name('admin.auth');
+Route::prefix('admin')->group(function() {
+    Route::middleware('admin')->group(function() {
+        Route::get('/dashboard', [AdminController::class, 'index'])->name('admin.index');
+        Route::post('logout', [AdminController::class,'logout'])->name('admin.logout');
+    });
+
+    Route::resource('categories', CategoryController::class, [
+        'names' => [
+            'index' => 'admin.categories.index',
+            'create' => 'admin.categories.create',
+            'show' => 'admin.categories.show',
+            'store' => 'admin.categories.store',
+            'edit' => 'admin.categories.edit',
+            'update' => 'admin.categories.update',
+            'destroy' => 'admin.categories.destroy',
+        ]
+    ]);
 });
