a292b53c24c8a92287a2b5c50529e35e034f0d66
Sallu <mohd.salman0306@gmail.com>
2024-06-27 10:49:20 +0400

    Implemented register and login functionality on client side

--- client/src/components/upload/Upload.jsx
+++ client/src/components/upload/Upload.jsx
@@ -5,7 +5,7 @@ import { FileUploader } from "react-drag-drop-files";
 import Spinner from "../layouts/Spinner";
 import axios from "axios";
 import { BASE_URL } from "../../helper/config";
-import {toast} from 'react-toastify'
+import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import useValidation from "../custom-hooks/useValidation";
 
@@ -22,104 +22,137 @@ const Upload = () => {
     const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
     const fileTypes = ["JPG", "PNG", "JPEG", "GIF"];
-    const [fileSizeError, setFileSizeError] = useState('');
+    const [fileSizeError, setFileSizeError] = useState("");
 
     const handleChange = (file) => {
-        setFileSizeError('')
+        setFileSizeError("");
         setPicture({
-            ...picture, file: file
+            ...picture,
+            file: file,
         });
     };
 
     const handleSizeError = () => {
-        setFileSizeError('The file size must not be greater than 2 mb');
-    }
+        setFileSizeError("The file size must not be greater than 2 mb");
+    };
 
     const storeImage = async (e) => {
-        e.preventDefault()
-        setErrors([])
-        setLoading(true)
-        const formData = new FormData()
-        formData.append('file', picture.file)
-        formData.append('title', picture.title)
-        formData.append('price', picture.price)
-        formData.append('category_id', picture.category_id)
-        formData.append('user_id', picture.user_id)
+        e.preventDefault();
+        setErrors([]);
+        setLoading(true);
+        const formData = new FormData();
+        formData.append("file", picture.file);
+        formData.append("title", picture.title);
+        formData.append("price", picture.price);
+        formData.append("category_id", picture.category_id);
+        formData.append("user_id", picture.user_id);
 
         try {
             const response = await axios.post(
-                `${BASE_URL}/store/picture`, formData
-            )
-            setLoading(false)
+                `${BASE_URL}/store/picture`,
+                formData
+            );
+            setLoading(false);
             toast.success(response.data.message, {
                 position: "top-right",
-            })
-            navigate('/')
+            });
+            navigate("/");
         } catch (error) {
-            setLoading(false)
+            setLoading(false);
             if (error?.response?.status === 422) {
-                setErrors(error.response.data.errors)
+                setErrors(error.response.data.errors);
             }
-            console.log(error)
+            console.log(error);
         }
-    }
+    };
 
-    return(
+    return (
         <div className="container">
             <div className="row my-5">
                 <div className="col-md-6 max-auto">
                     <div className="card">
                         <div className="card-header bg-white">
-                            <h5 className="text-center mt-4">
-                                Upload File
-                            </h5>
+                            <h5 className="text-center mt-4">Upload File</h5>
                         </div>
                         <div className="card-body">
-                            <form className="mt-5" onSubmit={(e) => storeImage(e)}>
+                            <form
+                                className="mt-5"
+                                onSubmit={(e) => storeImage(e)}
+                            >
                                 <div className="mb-3">
-                                    <label htmlFor="category" className="form-label">Category*</label>
-                                    <select
-                                    name="category_id"
-                                    id="category"
-                                    className="form-control"
-                                    value={picture.category_id}
-                                    onChange={(e) => setPicture({
-                                        ...picture, category_id:e.target.value
-                                    })}
+                                    <label
+                                        htmlFor="category"
+                                        className="form-label"
                                     >
-                                        <option value="" disabled>Choose Category</option>
-                                        {
-                                            categories?.map(category => (
-                                                <option
-                                                    key={category.id}
-                                                    value={category.id}>{category.name}</option>
-                                            ))
+                                        Category*
+                                    </label>
+                                    <select
+                                        name="category_id"
+                                        id="category"
+                                        className="form-control"
+                                        value={picture.category_id}
+                                        onChange={(e) =>
+                                            setPicture({
+                                                ...picture,
+                                                category_id: e.target.value,
+                                            })
                                         }
+                                    >
+                                        <option value="" disabled>
+                                            Choose Category
+                                        </option>
+                                        {categories?.map((category) => (
+                                            <option
+                                                key={category.id}
+                                                value={category.id}
+                                            >
+                                                {category.name}
+                                            </option>
+                                        ))}
                                     </select>
-                                    {useValidation(errors, 'category_id')}
+                                    {useValidation(errors, "category_id")}
                                 </div>
                                 <div className="mb-3">
-                                    <label htmlFor="title" className="form-label">Title*</label>
-                                    <input type="text" name="title" id="title"
-                                    className="form-control"
-                                    onChange={(e) => setPicture({
-                                        ...picture, title:e.target.value
-                                    })}
+                                    <label
+                                        htmlFor="title"
+                                        className="form-label"
+                                    >
+                                        Title*
+                                    </label>
+                                    <input
+                                        type="text"
+                                        name="title"
+                                        id="title"
+                                        className="form-control"
+                                        onChange={(e) =>
+                                            setPicture({
+                                                ...picture,
+                                                title: e.target.value,
+                                            })
+                                        }
                                     />
-                                    {useValidation(errors, 'title')}
+                                    {useValidation(errors, "title")}
                                 </div>
                                 <div className="mb-3">
-                                    <label htmlFor="price" className="form-label">Price*</label>
+                                    <label
+                                        htmlFor="price"
+                                        className="form-label"
+                                    >
+                                        Price*
+                                    </label>
                                     <input
                                         type="number"
                                         name="price"
                                         id="price"
                                         className="form-control"
-                                        onChange={(e) => setPicture({
-                                            ...picture, price:e.target.value
-                                        })}
+                                        onChange={(e) =>
+                                            setPicture({
+                                                ...picture,
+                                                price: e.target.value,
+                                            })
+                                        }
                                     />
-                                    {useValidation(errors, 'price')}
+                                    {useValidation(errors, "price")}
                                 </div>
                                 <div className="mb-3">
                                     <FileUploader
@@ -131,27 +164,34 @@ const Upload = () => {
                                         onSizeError={handleSizeError}
                                         classes="drop_area"
                                     />
-                                    {
-                                        fileSizeError && <div className="text-white my-2 rounded p-2 bg-danger">
+                                    {fileSizeError && (
+                                        <div className="text-white my-2 rounded p-2 bg-danger">
                                             {fileSizeError}
                                         </div>
-                                    }
-                                    {
-                                        picture?.file && <img
-                                        src={URL.createObjectURL(picture.file)}
-                                        alt="Picture"
-                                        width={150}
-                                        height={150}
-                                        className="rounded my-2" />
-                                    }
+                                    )}
+                                    {picture?.file && (
+                                        <img
+                                            src={URL.createObjectURL(
+                                                picture.file
+                                            )}
+                                            alt="Picture"
+                                            width={150}
+                                            height={150}
+                                            className="rounded my-2"
+                                        />
+                                    )}
                                 </div>
                                 <div className="mb-3">
-                                    {
-                                        loading ?
-                                            <Spinner />
-                                        :
-                                            <button type="submit" className="btn btn-sm btn-dark">Submit</button>
-                                    }
+                                    {loading ? (
+                                        <Spinner />
+                                    ) : (
+                                        <button
+                                            type="submit"
+                                            className="btn btn-sm btn-dark"
+                                        >
+                                            Submit
+                                        </button>
+                                    )}
                                 </div>
                             </form>
                         </div>
--- client/src/components/user/Login.jsx
+++ client/src/components/user/Login.jsx
@@ -1,10 +1,136 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import useValidation from "../custom-hooks/useValidation";
+import Spinner from "../layouts/Spinner";
+import axios from "axios";
+import { BASE_URL } from "../../helper/config";
+import { toast } from "react-toastify";
+import {
+    setCurrentUser,
+    setLoggedInOut,
+    setToken,
+} from "../../redux/slices/userSlice";
 
 export default function Login() {
-  return (
-    <div>
+    const [user, setUser] = useState({
+        email: "",
+        password: "",
+    });
+    const [errors, setErrors] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+    const { isLoggedIn } = useSelector((state) => state.user);
+    const dispatch = useDispatch();
 
-    </div>
-  )
+    useEffect(() => {
+        if (isLoggedIn) {
+            navigate("/");
+        }
+    }, [isLoggedIn, navigate]);
+
+    const loginUser = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+        setErrors([]);
+
+        try {
+            const response = await axios.post(`${BASE_URL}/user/login`, user);
+            setLoading(false);
+            if (!response.data.error) {
+                dispatch(setLoggedInOut(true));
+                dispatch(setCurrentUser(response.data.user));
+                dispatch(setToken(response.data.access_token));
+                toast.success(response.data.message, {
+                    position: "top-right",
+                });
+                navigate("/");
+            } else {
+                toast.error(response.data.error, {
+                    position: "top-right",
+                });
+            }
+        } catch (error) {
+            console.log(error);
+            setLoading(false);
+            if (error?.response?.status === 422) {
+                setErrors(error.response.data.errors);
+            }
+        }
+    };
+
+    return (
+        <div className="container">
+            <div className="row my-5">
+                <div className="col-md-6 mx-auto">
+                    <div className="card shadow-sm">
+                        <div className="card-header bg-white">
+                            <h5 className="text-center mt-2">Login</h5>
+                        </div>
+                        <div className="card-body">
+                            <form
+                                className="mt-5"
+                                onSubmit={(e) => loginUser(e)}
+                            >
+                                <div className="mb-3">
+                                    <label
+                                        htmlFor="email"
+                                        className="form-label"
+                                    >
+                                        Email*
+                                    </label>
+                                    <input
+                                        type="email"
+                                        name="email"
+                                        id="email"
+                                        className="form-control"
+                                        onChange={(e) =>
+                                            setUser({
+                                                ...user,
+                                                email: e.target.value,
+                                            })
+                                        }
+                                    />
+                                    {useValidation(errors, "email")}
+                                </div>
+                                <div className="mb-3">
+                                    <label
+                                        htmlFor="password"
+                                        className="form-label"
+                                    >
+                                        Password*
+                                    </label>
+                                    <input
+                                        type="password"
+                                        name="password"
+                                        id="password"
+                                        className="form-control"
+                                        onChange={(e) =>
+                                            setUser({
+                                                ...user,
+                                                password: e.target.value,
+                                            })
+                                        }
+                                    />
+                                    {useValidation(errors, "password")}
+                                </div>
+                                <div className="mb-3">
+                                    {loading ? (
+                                        <Spinner />
+                                    ) : (
+                                        <button
+                                            type="submit"
+                                            className="btn btn-sm btn-dark"
+                                        >
+                                            Submit
+                                        </button>
+                                    )}
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
-
