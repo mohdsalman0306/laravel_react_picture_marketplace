f8d39ad1804670be50dea1a065a10c51bb0a9fe8
Salman <mohd.salman0306@gmail.com>
2024-06-29 14:32:57 +0400

    removed unused code

--- client/src/components/upload/Upload.jsx
+++ client/src/components/upload/Upload.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import useCategories from "../custom-hooks/useCategories";
 import { useNavigate } from "react-router-dom";
 import { FileUploader } from "react-drag-drop-files";
