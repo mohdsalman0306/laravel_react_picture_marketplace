80ec9fb8f84e94ebcc4f650bfa64a46b03c6e458
Salman <mohd.salman0306@gmail.com>
2024-07-03 20:06:13 +0400

    Added Profile component

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -19,6 +19,7 @@ export default function Home() {
     useEffect(() => {
         setLoading(true)
         const fetchPictures = async () => {
+            setPicturesToShow(6)
             try {
                 if (categoryId) {
                     const response = await axios.get(`${BASE_URL}/pictures/category/${categoryId}`)
